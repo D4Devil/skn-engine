f295dddb074b60cb2ff8bba05705f0ff68f0a41d
D4Devil <dmdio1177@gmail.com>
2024-02-27 10:03:06 -0600

    Agregar archivos de proyecto.

---
+++ SKN-Engine/Game.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace SKN_Engine
+{
+    public class Game : GameWindow
+    {
+        public Game(int width = 480, int height = 480, string title = "Render Engine") : base(GameWindowSettings.Default, new NativeWindowSettings { ClientSize = (width, height), Title = title })
+        {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+        }
+    }
+}
---
+++ SKN-Engine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace SKN_Engine
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
